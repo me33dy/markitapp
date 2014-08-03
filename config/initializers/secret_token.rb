dc469f3106f6b46fc2d6e24d3bc3054916741a89
Eric Wu <ericwu@Erics-MacBook-Pro-2.local>
2014-08-03 14:25:44 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Markitapp::Application.config.secret_key_base = '8f66cc566648d190f0d04ce843e683e994e72bef6f2a6a33ea1e06e63e8fa07f9314d111adf5b0b5c144ce366940ab6e816c5ad12814537b14c4bb605f726605'
