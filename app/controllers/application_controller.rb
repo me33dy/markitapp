ad17f53fc6a8b028df33a8110c3b409bcffb78a9
Eric Wu <ericwu@Erics-MacBook-Pro-2.local>
2014-08-05 16:51:34 -0700

    add static pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,10 @@ class ApplicationController < ActionController::Base
 
   	before_filter :configure_permitted_parameters, if: :devise_controller?
 
-	def configure_permitted_parameters
-		  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username) }
-  devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :username) }
 
+	def configure_permitted_parameters
+		 devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username) }
+  		devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :username) }
 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Markitapp::Application.routes.draw do
+  
   devise_for :users
-  root :to => "home#index"
-  resources :user, only: [:show]
+  root :to => "users#show"
+  match '/welcome', to: 'static_pages#welcome', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
